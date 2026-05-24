2648ea7616cc58e9389289d4d58a3a8fca53c29a
meadows123 <zakmeadows1@hotmail.com>
2026-05-24 08:50:33 +0100

    Mark

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import AboutPage from '@/pages/AboutPage';
 import ChamonixRetreatPage from '@/pages/ChamonixRetreatPage';
 import AugustRetreatPage from '@/pages/AugustRetreatPage';
 import TermsAndConditionsPage from '@/pages/TermsAndConditionsPage';
+import BlogInternetTroubleshooting from '@/pages/BlogInternetTroubleshooting';
 
 function App() {
   // Enable smooth scrolling
@@ -38,6 +39,7 @@ function App() {
           <Route path="/terms" element={<TermsAndConditionsPage />} />
           <Route path="/booking" element={<BookingPage />} />
           <Route path="/booking/success" element={<BookingSuccessPage />} />
+          <Route path="/blog/internet-troubleshooting" element={<BlogInternetTroubleshooting />} />
         </Routes>
       </main>
       <Footer />
